205ed55a51f73d276813650389c90c984129e0c1
nishad2733 <istiannishad@gmail.com>
2024-01-12 23:23:51 +0600

    upload-2

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
 console.log('login hosse nah')
 
-document.getElementById('login-submit').addEventListener('click',function()
-{
-   const emailField = document.getElementById('user-email');
-    const email = emailField.value;
-    console.log(email);
+document.getElementById('login-submit').addEventListener('click',function () {
+        const emailField = document.getElementById('user-email');
+        const email = emailField.value;
 
-    const passwordField = document.getElementById('user-password');
-    const password = passwordField.value;
-    console.log(email, password);
-})
+        const passwordField = document.getElementById('user-password');
+        const password = passwordField.value;
+        console.log(email, password);
+
+
+
+    })
 
